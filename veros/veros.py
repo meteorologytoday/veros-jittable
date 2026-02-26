4424a2be53536e992e6d4972d861747aea663d3a
Tien-Yiao Hsu <tienyiao@pik-potsdam.de>
2026-02-26 14:18:01 +0100

    Remove sanity check statement for jittability

--- veros/veros.py
+++ veros/veros.py
@@ -308,8 +308,8 @@ class VerosSetup(metaclass=abc.ABCMeta):
         self.after_timestep(state)
 
         with state.timers["diagnostics"]:
-            if not numerics.sanity_check(state):
-                raise RuntimeError(f"solution diverged at iteration {vs.itt}")
+            #if not numerics.sanity_check(state):
+            #    raise RuntimeError(f"solution diverged at iteration {vs.itt}")
 
             isoneutral.isoneutral_diag_streamfunction(state)
             diagnostics.diagnose(state)
